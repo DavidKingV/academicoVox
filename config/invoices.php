ab2ea3e3a1a4aa9e137f93949bb40e7a6a83a615
thomas <thomas@debay.org>
2024-09-02 20:47:51 +0200

    fix open_basedir issue

--- config/invoices.php
+++ config/invoices.php
@@ -1,28 +1,30 @@
 <?php
 
-use LaravelDaily\Invoices\Classes\Seller;
-
 return [
     'date' => [
-        /**
+
+        /*
          * Carbon date format
          */
         'format' => 'Y-m-d',
-        /**
+
+        /*
          * Due date for payment since invoice's date.
          */
-        'pay_until_days' => 0,
+        'pay_until_days' => 7,
     ],
 
     'serial_number' => [
-        'series' => 'FC',
+        'series'   => 'AA',
         'sequence' => 1,
-        /**
+
+        /*
          * Sequence will be padded accordingly, for ex. 00001
          */
         'sequence_padding' => 5,
-        'delimiter' => '-',
-        /**
+        'delimiter'        => '.',
+
+        /*
          * Supported tags {SERIES}, {DELIMITER}, {SEQUENCE}
          * Example: AA.00001
          */
@@ -31,28 +33,33 @@ return [
 
     'currency' => [
         'code' => 'eur',
-        /**
+
+        /*
          * Usually cents
          * Used when spelling out the amount and if your currency has decimals.
          *
          * Example: Amount in words: Eight hundred fifty thousand sixty-eight EUR and fifteen ct.
          */
         'fraction' => 'ct.',
-        'symbol' => '€',
-        /**
+        'symbol'   => '€',
+
+        /*
          * Example: 19.00
          */
         'decimals' => 2,
-        /**
+
+        /*
          * Example: 1.99
          */
         'decimal_point' => '.',
-        /**
+
+        /*
          * By default empty.
          * Example: 1,999.00
          */
         'thousands_separator' => '',
-        /**
+
+        /*
          * Supported tags {VALUE}, {SYMBOL}, {CODE}
          * Example: 1.99 €
          */
@@ -61,22 +68,22 @@ return [
 
     'paper' => [
         // A4 = 210 mm x 297 mm = 595 pt x 842 pt
-        'size' => 'a4',
+        'size'        => 'a4',
         'orientation' => 'portrait',
     ],
 
     'disk' => 'local',
 
     'seller' => [
-        /**
+        /*
          * Class used in templates via $invoice->seller
          *
          * Must implement LaravelDaily\Invoices\Contracts\PartyContract
          *      or extend LaravelDaily\Invoices\Classes\Party
          */
-        'class' => Seller::class,
+        'class' => \LaravelDaily\Invoices\Classes\Seller::class,
 
-        /**
+        /*
          * Default attributes for Seller::class
          */
         'attributes' => [
@@ -93,5 +100,14 @@ return [
                 'CIF' => config('academico.company_id'),
             ],
         ],
+    ],
+
+    'dompdf_options' => [
+        'enable_php' => true,
+        /**
+         * Do not write log.html or make it optional
+         *  @see https://github.com/dompdf/dompdf/issues/2810
+         */
+        'logOutputFile' => '',
     ],
 ];
